5d3288459d5bc373ef2715502f57a973c63b71a1
Chase Cassels <chasecassels@gmail.com>
2025-12-01 14:26:47 -0800

    small changes

--- file-extraction/get-nyc.py
+++ file-extraction/get-nyc.py
@@ -8,6 +8,9 @@ from shapely.geometry import Point
 nyc_boundary = gpd.read_file("nybb_25c/nybb.shp")
 nyc_boundary = nyc_boundary.to_crs(epsg=4326)
 nyc_boundary = nyc_boundary.union_all()
+target_folder = "../source-data"
+nyc_files = []
+yrs = []
 
 #test projection
 if nyc_boundary.contains(Point(-74.0060, 40.7128)):
@@ -15,20 +18,14 @@ if nyc_boundary.contains(Point(-74.0060, 40.7128)):
 else:
     print("Projection error", flush=True)
 
-target_folder = "../source-data"
-nyc_files = []
-yrs = []
-
 #retrieve ID's for individual years from parent project
 PARENT_ID = "5dp8e"
 parent_url = f"https://api.osf.io/v2/nodes/{PARENT_ID}/children/"
-
 while parent_url:
     resp = requests.get(parent_url).json()
     for child in resp["data"]:
         yrs.append(child["id"])
     parent_url = resp["links"].get("next")
-
 
 #file download function
 def download_file(file):
@@ -39,7 +36,6 @@ def download_file(file):
     with open(output_path, "wb") as f:
         f.write(content)
         print("Saved " + filename + " in " + target_folder, flush=True)
-
 
 #retrieve all files with location in nyc boundary
 for i in range(len(yrs)):
