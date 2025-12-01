60a5023af7c3cb97014fb723e3170dd4d3507bdf
Chase Cassels <chasecassels@gmail.com>
2025-12-01 13:30:05 -0800

    testing gitignore

--- file-extraction/load-csvs.py
+++ file-extraction/load-csvs.py
@@ -6,6 +6,7 @@ folder_path = "../source-data"
 db_path = "../database/weather-data.db"
 conn = sqlite3.connect(db_path)
 
+#import all csvs into sqlite db
 for filename in os.listdir(folder_path):
     if filename.endswith(".csv"):
         file_path = os.path.join(folder_path, filename)
