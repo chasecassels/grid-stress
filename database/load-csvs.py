d67f05ee78864db0b6600867ec4629fbd6877788
Chase Cassels <chasecassels@gmail.com>
2025-12-01 13:02:57 -0800

    script to load into sqlite db finished

---
+++ database/load-csvs.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import sqlite3
+import os
+
+folder_path = "../source-data"
+db_path = "weather-data.db"
+conn = sqlite3.connect(db_path)
+
+for filename in os.listdir(folder_path):
+    if filename.endswith(".csv"):
+        file_path = os.path.join(folder_path, filename)
+        df = pd.read_csv(file_path)
+        table_name = os.path.splitext(filename)[0]
+        df.to_sql(table_name, conn, if_exists="replace", index=False)
+        print(f"Imported '{filename}' into table '{table_name}'")
+
+conn.commit()
+conn.close()
+print("All files imported successfully!")
