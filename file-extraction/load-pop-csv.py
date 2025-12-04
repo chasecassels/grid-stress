466a053c065b853b920adcd8aab00b8ea1d17eb0
Chase Cassels <chasecassels@gmail.com>
2025-12-03 21:40:55 -0800

    added population table cleansing during import

--- file-extraction/load-pop-csv.py
+++ file-extraction/load-pop-csv.py
@@ -8,23 +8,34 @@ conn = sqlite3.connect(db_path)
 file = 'New_York_City_Population_by_Borough,_1950_-_2040_20251203.csv'
 df = pd.DataFrame()
 
+#transpose and format input data
+def process_input(df) -> pd.DataFrame:
+    df = df.drop(df.columns[0], axis = 1)
+    df = df.drop(df.columns[2::2], axis = 1)
+    print(df)
+    df_with_names = df.copy()
+    df = df_with_names.T
+    df = df.drop(df.columns[[1,5]], axis = 1)
+    print(df)
+    return df
+
 #import population csv into sqlite db
 for filename in os.listdir(folder_path):
     if filename == file:
         file_path = os.path.join(folder_path, filename)
         df = pd.read_csv(file_path)
-        df = df.drop(df.columns[0], axis = 1)
-        df = df.drop(df.columns[2::2], axis = 1)
-        df = interpolate(df)
+        df = process_input(df)
         table_name = os.path.splitext(filename)[0]
         parts = table_name.split("_")
         output_table_name = "nyc_population"
-        df.to_sql(output_table_name, conn, if_exists="replace", index=False)
+        df.index.values[0] = "Decade"
+        df.to_sql(output_table_name, conn, if_exists="replace", index=True, index_label = 'Year')
         print(f"Imported '{filename}' into table '{output_table_name}'", flush=True)
 
+
 #temporal downscaling to minutes (unnecessary but why not)
-def interpolate(df) -> DataFrame:
-    
+#def interpolate(df) -> DataFrame:
+
 
 
 
